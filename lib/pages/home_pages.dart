b413e6be6442d6ce2df5e034708133560b86d43f
RODULO <rodu430@gmail.com>
2025-07-18 00:40:51 -0500

    realizando interfaz de pokemones

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedexg12/apiservices/pokemon_api.dart';
 import 'package:pokedexg12/apiservices/pokemon_api_service_http.dart';
 import 'package:pokedexg12/pages/models/pokemon_reponse.dart';
+import 'package:pokedexg12/widgets/pokemon_widget.dart';
 
 class HomePages extends StatefulWidget {
   const HomePages({super.key});
@@ -13,17 +14,26 @@ class HomePages extends StatefulWidget {
 
 class _HomePagesState extends State<HomePages> {
   late PokemonApi _pokemonApi;
+  PokemonReponse? _pokemonReponse; 
 
   void initPokemonRestClient() {
     final dio = Dio(
-      BaseOptions(responseType: ResponseType.plain) //forzamos a recibir un texto plano como lo recivimos en la api de pokemon
+      BaseOptions(
+        responseType: ResponseType.plain
+      ) //forzamos a recibir un texto plano como lo recivimos en la api de pokemon
     );
     _pokemonApi = PokemonApi(dio);
+  }
+
+  Future<void> fetchPokemonReponse() async {
+    _pokemonReponse = await PokemonApiServiceHttp().getPokemonReponse();
+    setState(() {});
   }
 
   @override
   void initState() {
     initPokemonRestClient();
+    fetchPokemonReponse();
     super.initState();
   }
 
@@ -39,8 +49,36 @@ class _HomePagesState extends State<HomePages> {
             final PokemonReponse? pokemones = await PokemonApiServiceHttp()
               .getPokemonReponse();
             print(pokemones!.pokemon[0].name);
-          }
+          },
         ),
+        body:_pokemonReponse == null
+            ?Center(child: CircularProgressIndicator(),)
+            :Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    "tu pokedex",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+                    ),
+                  Text(
+                    "¿Quien es ese pokemon? Busca el pokemon de tu preferencia y observa sus cualidades" 
+                  ),
+                  Expanded(
+                    child: GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 0.93
+                      ),
+                      children: _pokemonReponse!.pokemon
+                          .map((pokemon) => PokemonWidget(pokemon))
+                          .toList(),
+                  ))
+                ],
+              ),
+            ),
       ),
     );
   }
---
+++ lib/widgets/pokemon_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
+import 'package:pokedexg12/pages/models/pokemon_reponse.dart';
+
+class PokemonWidget extends StatefulWidget {
+  Pokemon pokemon;
+  PokemonWidget(this.pokemon);
+
+  @override
+  State<PokemonWidget> createState() => _PokemonWidgetState();
+}
+
+class _PokemonWidgetState extends State<PokemonWidget> {
+    late Future<Color?> _dominantColor;
+
+  Future <Color?> _getDominanColor(String urlImage) async {
+    final imageProvider = NetworkImage(urlImage);
+    final paletteGenerator = await PaletteGenerator.fromImageProvider(
+      imageProvider,
+    );
+    return paletteGenerator.vibrantColor?.color;
+  }
+
+  @override
+  void initState() {
+    _dominantColor = _getDominanColor(widget.pokemon.img);
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _dominantColor,
+      builder: (context, AsyncSnapshot) {
+
+        if(!AsyncSnapshot.hasData) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+        final Color dominantColor = AsyncSnapshot.data?? Colors.white;
+        return Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: dominantColor.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text("Bulbasaur"), Icon(Icons.favorite_border)],
+              ),
+              Image.network(widget.pokemon.img),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
